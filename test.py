92f036c1b4336c672800440f5781efd2c028f776
sandero16 <sander.thierens@hotmail.com>
2019-05-21 10:45:27 +0100

    script

---
+++ test.py
@@ -0,0 +1,25 @@
+from mpi4py import MPI
+import numpy
+
+def computepi ( samples ) :
+    count=0
+    for x , y in samples :
+        if x**2 + y**2 <= 1 :
+            count += 1
+            pi = 4*float (count)/ len ( samples )
+            return pi
+
+comm = MPI.COMMWORLD
+nprocs = comm.Get_size()
+myrank = comm. Get_rank ( )
+if myrank == 0 :
+    N = 100000 // nprocs
+    samples = numpy.random.random((nprocs,N,2))
+else :
+    samples = None
+    samples = comm.scatter( samples , root=0)
+    mypi = computepi(samples)/nprocs
+    pi = comm.reduce(mypi , root=0)
+if myrank == 0 :
+    error = abs ( pi - numpy . pi )
+    print (" pi i s approximately %.16 f , e r r o r i s %.16 f " % ( pi , error ) )
