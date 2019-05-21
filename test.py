61527f43de424d75219d4228ce6ec37b773362c2
sandero16 <sander.thierens@hotmail.com>
2019-05-21 11:39:44 +0100

    fix

--- test.py
+++ test.py
@@ -26,6 +26,7 @@ pi = comm.reduce(mypi , root=0)
 
 if myrank == 0 :
     error = abs (pi-numpy.pi )
-    print (" pi i s approximately %.16 f , e r r o r i s %.16 f " % ( pi , error ) )
+    //print (" pi i s approximately %.16f , error %.16f " % ( pi , error ) )
+    print "pi is approcximately", pi, "met error", error
 end=time.time()
 print "time ",(end-start)
